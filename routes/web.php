cafdffa6bee8fd5193457e7ac6099883cd51079a
AaronCHH <aaronhsu219@gmail.com>
2021-02-25 18:18:31 +0800

    basic pages with controller and routing

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\PagesController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-  return view('index');
-});
+// Route::get('/', function () {
+//   return view('index');
+// });
+Route::get('/', [PagesController::class, 'home']);
+Route::get('/about', [PagesController::class, 'about']);
